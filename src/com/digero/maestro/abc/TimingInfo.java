28d593bb0d0da4d399db85f4361ef20f7a67a7b0
Nikolai V. Chr <emptyyetfull@gmail.com>
2021-09-01 09:20:23 +0200

    When breaking up long notes, allow them only to be 5s instead of 6s, as some samples are only around 5s long.

--- src/com/digero/maestro/abc/TimingInfo.java
+++ src/com/digero/maestro/abc/TimingInfo.java
@@ -8,7 +8,7 @@ public class TimingInfo
 	public static final long ONE_SECOND_MICROS = 1000000;
 	public static final long ONE_MINUTE_MICROS = 60 * ONE_SECOND_MICROS;
 	//public static final long SHORTEST_NOTE_MICROS = 60001;
-	public static final long LONGEST_NOTE_MICROS = ONE_MINUTE_MICROS / 10;
+	public static final long LONGEST_NOTE_MICROS = ONE_MINUTE_MICROS / 12;// reduced to 5s from 6s due to some samples are shorter than 6s.
 	public static final int MAX_TEMPO_BPM = (int) (ONE_MINUTE_MICROS / getShortestNoteMicros(125));
 	public static final int MIN_TEMPO_BPM = (int) ((ONE_MINUTE_MICROS + LONGEST_NOTE_MICROS / 2) / LONGEST_NOTE_MICROS); // Round up
 
